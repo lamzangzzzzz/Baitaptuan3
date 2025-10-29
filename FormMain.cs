50371ff4ba3397c9798569c511cb9a2a1571376c
lamzangzzzzz <lamzangzzzzz@gmail.com>
2025-10-29 20:37:07 +0700

    Update FormMain.cs

--- FormMain.cs
+++ FormMain.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Windows.Forms;
 
 namespace Baitaptuan3_cau3
@@ -21,22 +21,34 @@ namespace Baitaptuan3_cau3
             toolStripStatusLabelTime.Text = DateTime.Now.ToString("HH:mm:ss dd/MM/yyyy");
         }
 
-        private void sảnPhẩmToolStripMenuItem_Click(object sender, EventArgs e)
+        private void MdiShow<T>() where T : Form, new()
         {
-            FormSanPham f = new FormSanPham();
+            foreach (Form frm in this.MdiChildren)
+            {
+                if (frm is T)
+                {
+                    frm.Activate();
+                    return;
+                }
+            }
+            T f = new T();
             f.MdiParent = this;
             f.Show();
+        }
+
+        private void sảnPhẩmToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MdiShow<FormSanPham>();
             toolStripStatusLabelForm.Text = "Form: Sản phẩm";
         }
 
-    
         private void hóaĐơnToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            FormHoaDon f = new FormHoaDon();
-            f.MdiParent = this;
-            f.Show();
+            MdiShow<FormHoaDon>();
             toolStripStatusLabelForm.Text = "Form: Hóa đơn";
         }
+
+        
 
         private void thoátToolStripMenuItem_Click(object sender, EventArgs e)
         {
@@ -44,7 +56,25 @@ namespace Baitaptuan3_cau3
                 == DialogResult.Yes)
                 Application.Exit();
         }
-        
 
+        private void đăngNhậpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            toolStripStatusLabelUser.Text = "Người dùng: Admin";
+        }
+
+        private void đăngXuấtToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            toolStripStatusLabelUser.Text = "Người dùng: (Chưa đăng nhập)";
+        }
+
+        private void giớiThiệuToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Ứng dụng Quản lý cửa hàng mini.\nUEH - WinForms", "Giới thiệu");
+        }
+
+        private void liênHệToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Email hỗ trợ: tuannm@ueh.edu.vn", "Liên hệ");
+        }
     }
 }
